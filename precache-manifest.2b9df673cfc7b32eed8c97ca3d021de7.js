9fda04ade940c91b04c09eb6868bedd7702add1b
hosam faour <hosamf1992@gmail.com>
2019-12-31 23:15:41 +0200

    Updates

---
+++ precache-manifest.2b9df673cfc7b32eed8c97ca3d021de7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb230122facf002c381c3b45eb93860",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/index.html"
+  },
+  {
+    "revision": "6d4ade1f8b287bef4243",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/css/main.ff21d55d.chunk.css"
+  },
+  {
+    "revision": "7e778e2f247ed195a94f",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/js/2.9d5bc014.chunk.js"
+  },
+  {
+    "revision": "1625cc70715175ea2fe2e233d8f5a6ad",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/js/2.9d5bc014.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d4ade1f8b287bef4243",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/js/main.e786dc34.chunk.js"
+  },
+  {
+    "revision": "aa536192f5b83d808edb",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/js/runtime-main.c63a2e0c.js"
+  },
+  {
+    "revision": "faf314392e876e6a1e6eae2e594105dd",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/media/avatar.faf31439.png"
+  },
+  {
+    "revision": "dc30b176b601a7cb81d33a6ee8af6670",
+    "url": "​https://hosamf1992.github.io/Mister-Coin/static/media/background.dc30b176.jpg"
+  }
+]);
